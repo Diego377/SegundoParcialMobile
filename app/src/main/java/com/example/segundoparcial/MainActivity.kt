85540d7fbeb328f93114da6d6fba355f182b7f09
Diego377 <ddcabrera69@gmail.com>
2023-03-30 21:27:28 -0400

    recycler view

--- app/src/main/java/com/example/segundoparcial/MainActivity.kt
+++ app/src/main/java/com/example/segundoparcial/MainActivity.kt
@@ -2,10 +2,51 @@ package com.example.segundoparcial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var recyclerView: RecyclerView
+//    lateinit var list : ArrayList<Post>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val restApiAdapter = RestApiAdapter()
+        val endPoint = restApiAdapter.connectionApi()
+        val bookResponseCall = endPoint.getAllPost()
+        val list = arrayListOf<Post>()
+//        var list = emptyList<Post>()
+        bookResponseCall.enqueue( object : Callback<List<Post>> {
+            override fun onFailure(call: Call<List<Post>>, t: Throwable) {
+                t?.printStackTrace()
+            }
+
+            override fun onResponse(call: Call<List<Post>>, response: Response<List<Post>>) {
+                val posts = response?.body()
+
+                posts?.forEach{
+                    list.add(Post(it.userId, it.id, it.title, it.body))
+                    recyclerView.adapter = PostListAdapter(list, this)
+                }
+
+//                Log.d("RESP POST", Gson().toJson(posts))
+//                posts?.forEach {
+//                    Log.d("RESP POST", it.body)
+            }
+        })
+        val linearLayoutManager = LinearLayoutManager(this)
+
+        linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
+
+        recyclerView = findViewById(R.id.recycler_view)
+        recyclerView.layoutManager = linearLayoutManager
+//        recyclerView.adapter = PostListAdapter(list, this)
+
     }
+
 }
