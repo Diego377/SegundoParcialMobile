01f6e7551281102eb1c0f94f49d99bc30cce9b03
Diego377 <ddcabrera69@gmail.com>
2023-03-30 21:51:27 -0400

    base de datos

--- app/src/main/java/com/example/segundoparcial/MainActivity.kt
+++ app/src/main/java/com/example/segundoparcial/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.segundoparcial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -11,7 +14,6 @@ import retrofit2.Response
 class MainActivity : AppCompatActivity() {
 
     lateinit var recyclerView: RecyclerView
-//    lateinit var list : ArrayList<Post>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,7 +22,6 @@ class MainActivity : AppCompatActivity() {
         val endPoint = restApiAdapter.connectionApi()
         val bookResponseCall = endPoint.getAllPost()
         val list = arrayListOf<Post>()
-//        var list = emptyList<Post>()
         bookResponseCall.enqueue( object : Callback<List<Post>> {
             override fun onFailure(call: Call<List<Post>>, t: Throwable) {
                 t?.printStackTrace()
@@ -46,6 +47,17 @@ class MainActivity : AppCompatActivity() {
         recyclerView = findViewById(R.id.recycler_view)
         recyclerView.layoutManager = linearLayoutManager
 //        recyclerView.adapter = PostListAdapter(list, this)
+
+        GlobalScope.launch {
+            val bookDao = AppRoomDatabase.getDatabase(applicationContext).bookDato()
+            val repository = BookRepository(bookDao)
+            repository.insert(Book("the best seller: Android"))
+            val lista = repository.getListBooks()
+            lista.forEach {
+                Log.d("DBTEST","Id book = ${it.id}, Title: ${it.title}")
+            }
+        }
+
 
     }
 
